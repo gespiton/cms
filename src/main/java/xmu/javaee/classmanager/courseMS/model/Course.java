f438bb301d0271dee8ae083044c62c647db136f0
jim shin <shin@jims-iMac.local>
2017-10-05 11:49:22 +0800

    init

---
+++ src/main/java/xmu/javaee/classmanager/courseMS/model/Course.java
@@ -0,0 +1,31 @@
+package xmu.javaee.classmanager.courseMS.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/test/java/xmu/javaee/classmanager/ClassManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package xmu.javaee.classmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClassManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
