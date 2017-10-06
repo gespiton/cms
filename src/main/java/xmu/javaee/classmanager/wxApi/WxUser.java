19fe0910eb8b002ee170f8c6513f05653273dc8e
jim shin <shin@jims-iMac.local>
2017-10-06 18:12:17 +0800

    fetch user seesion openid

---
+++ src/main/java/xmu/javaee/classmanager/wxApi/WxUser.java
@@ -0,0 +1,37 @@
+package xmu.javaee.classmanager.wxApi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("api/v1/user/")
+public class WxUser {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public WxUser(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    @ResponseBody
+    public String addCourse(@RequestParam String code) {
+        System.out.println("===> " + code);
+        getSession(code);
+        return "received code";
+    }
+
+    private WxSession getSession(String code) {
+        WxSession session = restTemplate.getForObject(
+                "https://api.weixin.qq.com/sns/jscode2session?" +
+                        "appid=wx6ab1e6c4ae4c17ef&" +
+                        "secret=3404d5c3fcfd0c557378008628c7e0d6&" +
+                        "js_code=" + code +
+                        "&grant_type=authorization_code", WxSession.class);
+        System.out.println(session);
+        return session;
+    }
+
+}
