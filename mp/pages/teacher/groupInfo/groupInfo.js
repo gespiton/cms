81d6897275267f4d120b115a1885f1429b0f11ad
caixiaocong <caistrong@outlook.com>
2017-12-01 19:54:27 +0800

    先push一份能跑的，js api 传参之类的写得很乱

--- mp/pages/teacher/groupInfo/groupInfo.js
+++ mp/pages/teacher/groupInfo/groupInfo.js
@@ -1,18 +1,37 @@
 // pages/teacher/groupInfo/groupInfo.js
+import apiGroup from '../../../utils/groupInfoApi.js'
+import apiCall from '../../../utils/rollCallList.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+      groups:[],
+      latesArr:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+    apiGroup.getGroupsByClass(function(value){
+      var groupsShowData = []
+      for(let i=0;i<value.length;i++){
+        groupsShowData.push({
+          ...value[i],
+          shown:false
+        })
+      }
+      that.setData({
+        groups: groupsShowData
+      })
+    });
+    let lates = apiCall.getLateStudents();
+    this.setData({
+      latesArr:lates
+    })
   },
 
   /**
@@ -62,5 +81,61 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  toggle:function(e){
+    let tapId = e.currentTarget.dataset.groupid
+    let list = this.data.groups
+
+    for (let i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == tapId) {
+        list[i].shown = !list[i].shown
+      }
+    }
+    this.setData({
+      groups: list
+    });
+  },
+
+  addMember:function(e){
+    console.log(e)
+
+    var that = this;
+
+
+    let tapId = e.currentTarget.dataset.groupid
+    let list = this.data.groups
+
+    let templist = apiCall.getLateStudents()
+    let latelist = []
+    for(let i=0; i<templist.length;i++){
+      latelist.push(templist[i].name)
+    }
+    
+    wx.showActionSheet({
+      itemList: latelist,
+      success: function (res) {
+
+        console.log(res.tapIndex)
+        let people = templist[res.tapIndex]
+        console.log(people)
+        for (let i = 0, len = list.length; i < len; ++i) {
+          if (list[i].id == tapId) {
+            console.log(list[i])
+            list[i].members.push(people)
+          }
+        }
+        templist.splice(res.tapIndex,1)
+
+        that.setData({
+          groups: list,
+          latesArr: templist
+        })
+      },
+      fail: function (res) {
+        console.log(res.errMsg)
+      }
+    })
   }
+
 })
