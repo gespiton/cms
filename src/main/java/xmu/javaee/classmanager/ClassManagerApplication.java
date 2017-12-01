6e1e40750d01ae66ac136cd5c8fd96a4f1e1ff27
gespiton <1044750905@qq.com>
2017-12-02 00:43:57 +0800

    ammend

--- src/main/java/xmu/javaee/classmanager/ClassManagerApplication.java
+++ src/main/java/xmu/javaee/classmanager/ClassManagerApplication.java
@@ -3,9 +3,13 @@ package xmu.javaee.classmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author: Sher
+ */
 @SpringBootApplication
 public class ClassManagerApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(ClassManagerApplication.class, args);
     }
 }
