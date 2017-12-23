1e8dbe1c63ce32d861ad5c0ad04ab69ff05490dc
gespiton <1044750905@qq.com>
2017-12-23 17:45:10 +0800

    almost complete topic mapper

--- src/main/java/xmu/crms/serviceImpl/TopicServiceImpl.java
+++ src/main/java/xmu/crms/serviceImpl/TopicServiceImpl.java
@@ -1,5 +1,9 @@
 package xmu.crms.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import xmu.crms.dao.TopicDao;
+import xmu.crms.entity.SeminarGroupTopic;
 import xmu.crms.entity.Topic;
 import xmu.crms.exception.TopicNotFoundException;
 import xmu.crms.service.TopicService;
@@ -10,24 +14,59 @@ import java.util.List;
 /**
  * @author shin
  */
-public class TopicServiceImpl implements TopicService{
+@Service
+public class TopicServiceImpl implements TopicService {
+    @Autowired
+    TopicDao topicDao;
+
     @Override
     public Topic getTopicByTopicId(BigInteger topicId) throws TopicNotFoundException, IllegalArgumentException {
-        return null;
+        if (topicId == null) {
+            throw new IllegalArgumentException("id should be null");
+        }
+
+        try {
+            Topic t = topicDao.getTopicById(BigInteger.valueOf(12));
+            if (t == null) {
+                throw new TopicNotFoundException();
+            }
+            return t;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
-    public Boolean updateTopicByTopicId(BigInteger topicId, Topic topic) throws TopicNotFoundException, IllegalArgumentException {
-        return null;
+    public void updateTopicByTopicId(BigInteger topicId, Topic topic) throws TopicNotFoundException, IllegalArgumentException {
+        if (topicId == null) {
+            throw new IllegalArgumentException("id should be null");
+        }
+
+        try {
+            Topic t = topicDao.getTopicById(BigInteger.valueOf(12));
+            if (t == null) {
+                throw new TopicNotFoundException();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public Boolean deleteTopicByTopicId(BigInteger topicId, BigInteger seminarId) throws IllegalArgumentException {
-        return null;
+    public void deleteTopicByTopicId(BigInteger topicId) throws IllegalArgumentException {
+
+        try {
+            this.getTopicByTopicId(BigInteger.valueOf(1));
+        } catch (TopicNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<Topic> listTopicBySeminarId(BigInteger seminarId) throws IllegalArgumentException {
+
+
         return null;
     }
 
@@ -37,17 +76,22 @@ public class TopicServiceImpl implements TopicService{
     }
 
     @Override
-    public Boolean deleteTopicById(BigInteger groupId, BigInteger topicId) throws IllegalArgumentException {
+    public void deleteSeminarGroupTopicById(BigInteger groupId, BigInteger topicId) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void deleteSeminarGroupTopicByTopicId(BigInteger topicId) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public SeminarGroupTopic getSeminarGroupTopicById(BigInteger topicId, BigInteger groupId) throws IllegalArgumentException {
         return null;
     }
 
     @Override
-    public Boolean deleteSeminarGroupTopicByTopicId(BigInteger topicId) throws IllegalArgumentException {
-        return null;
-    }
+    public void deleteTopicBySeminarId(BigInteger seminarId) throws IllegalArgumentException {
 
-    @Override
-    public Boolean deleteTopicBySeminarId(BigInteger seminarId) throws IllegalArgumentException {
-        return null;
     }
 }
